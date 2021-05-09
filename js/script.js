74d0c5291e7bd254d52dd879b61c039942e05a60
Ilya <ilyapavlushchik221@gmail.com>
2021-05-09 21:23:56 +0300

    Сделал 3 урок
    Доработал 3 урок
    
    -Добавил удаленные ранее строки из 2 урока
    -добавил пару функций из 4

--- js/script.js
+++ js/script.js
@@ -1,20 +1,50 @@
-let money = 132,
-    income = "Стипендия",
-    addExpenses = "Еда, автобус, универ",
-    deposit = false,
-    mission = 600,
-    period = 12,
-    budgetDay = money / 30;
+'use strict'
 
-console.log (typeof money);
-console.log (typeof income);
-console.log (typeof deposit);
+let money = +prompt('Сколько ты зарабатываешь?'),
+    income = 'Стипендия',
+	addExpenses = prompt(
+		'Перечислите возможные расходы за рассчитываемый период через запятую'
+	),
+	deposit = confirm('Есть ли у вас депозит в банке?'),
+	mission = 150000,
+	expenses1 = prompt('Введите обязательную статью расходов?'),
+	amount1 = +prompt('Во сколько это обойдется?'),
+	expenses2 = prompt('Введите обязательную статью расходов?'),
+	amount2 = +prompt('Во сколько это обойдется?'),
+	budgetMonth = money - amount1 - amount2,
+	period = Math.ceil(mission / budgetMonth),
+	budgetDay = Math.floor(budgetMonth / 30);
+
+const showTypeOf = function (data) {
+    console.log(data, typeof data);
+}
+showTypeOf(money);
+showTypeOf(deposit);
+showTypeOf(income);
 
 console.log(addExpenses.length);
 
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + money + " рублей");
+console.log('Дневной бюджет: ', budgetDay);
+console.log('Бюджет на месяц: ', budgetMonth);
 
-console.log(addExpenses.toLowerCase().split(", "));
+console.log('Цель заработать ' + mission + ' рублей');
+console.log('Период равен ' + period + ' месяцев');
 
-console.log ("Дневной бюджет: ", budgetDay);
+const showMoneyStatus = function () {
+	switch (true) {
+		case budgetDay >= 1200:
+			return('У вас высокий уровень дохода');
+			break;
+		case budgetDay >= 600 && budgetDay <= 1200:
+			return('У вас средний уровень дохода');
+			break;
+		case budgetDay >= 0 && budgetDay <= 600:
+			return('К сожалению у вас уровень дохода ниже среднего');
+			break;
+		default:
+			return('Что то пошло не так');
+			break;
+	}
+}
+
+console.log(showMoneyStatus());
