f2d35392a25c1c5c1c0e2725f84023ec0192e4b4
Ilya <ilyapavlushchik221@gmail.com>
2021-05-09 21:23:56 +0300

    Сделал 3 урок
    Доработал 3 урок
    
    -Добавил удаленные ранее строки из 2 урока
    -добавил пару функций из 4
    -Изменил некоторые переменные на const
    -Изменил функции на стрелочные

--- js/hardTusk.js
+++ js/hardTusk.js
@@ -1,13 +1,39 @@
-const num = 266219;
-    
-const sumNumber = Math.pow (
-    num
-    .toString()
-    .split("")
-    .reduce((prev, next) => prev * Number(next), 1),
-    3
-)
-.toString()
-.slice(0, 2);
+'use strict';
 
-console.log("result: ", sumNumber);
+const lang = prompt("Введите язык ru или en");
+let arr;
+const namePerson = prompt("Введите имя");
+
+//Оператор if
+if (lang == "ru") {
+    arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+}
+else if (lang == "en") {
+	arr = ["mn", "tu", "wn", "th", "fr", "st", "sn"];
+}
+console.log ("Месяцы: ", arr);
+
+//Оператор switch
+
+switch (lang) {
+    case "ru":
+        arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+    break;
+    case "en":
+	    arr = ["mn", "tu", "wn", "th", "fr", "st", "sn"];
+    break;
+}
+console.log ("Месяцы: ", arr);
+
+//Многомерный массив
+
+arr = {
+	"ru":["пн", "вт", "ср","чт", "пт", "сб", "вс"],
+	"en":["mn", "tu", "wn", "th", "fr", "st", "sn"],
+};
+
+console.log ("Месяцы: ", arr[lang]);
+
+const result = namePerson === "Артем" ? "директор" : namePerson === "Максим" ? "преподаватель" : "студент";
+
+console.log (result);
--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
-let money = 132,
-    income = "Стипендия",
-    addExpenses = "Еда, автобус, универ",
-    deposit = false,
-    mission = 600,
-    period = 12,
-    budgetDay = money / 30;
+'use strict'
 
-console.log (typeof money);
-console.log (typeof income);
-console.log (typeof deposit);
+const mission = 150000;
+const income = 'Стипендия';
+const money = +prompt('Сколько ты зарабатываешь?');
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const deposit = confirm('Есть ли у вас депозит в банке?');
+const expenses1 = prompt('Введите обязательную статью расходов?');
+const amount1 = +prompt('Во сколько это обойдется?');
+const expenses2 = prompt('Введите обязательную статью расходов?');
+const amount2 = +prompt('Во сколько это обойдется?');
+
+const showTypeOf = (data) => {
+    console.log(data, typeof data);
+}
+showTypeOf(money);
+showTypeOf(deposit);
+showTypeOf(income);
 
 console.log(addExpenses.length);
 
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + money + " рублей");
+const budgetMonth = money - amount1 - amount2;
+const budgetDay = Math.floor(budgetMonth / 30);
+console.log('Дневной бюджет: ', budgetDay);
+console.log('Бюджет на месяц: ', budgetMonth);
 
-console.log(addExpenses.toLowerCase().split(", "));
+console.log('Цель заработать ' + mission + ' рублей');
+const period = Math.ceil(mission / budgetMonth);
+console.log('Период равен ' + period + ' месяцев');
 
-console.log ("Дневной бюджет: ", budgetDay);
+const showMoneyStatus = () => {
+		if (budgetDay >= 1200)
+			return('У вас высокий уровень дохода');
+		else if (budgetDay >= 600 && budgetDay <= 1200)
+			return('У вас средний уровень дохода');
+		else if(budgetDay >= 0 && budgetDay <= 600)
+			return('К сожалению у вас уровень дохода ниже среднего');
+		else
+			return('Что то пошло не так');
+}
+
+console.log(showMoneyStatus());
