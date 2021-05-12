1aeaaf1342fe59e54322f2a9779e714032353393
Ilya <ilyapavlushchik221@gmail.com>
2021-05-12 14:00:59 +0300

    Сделал 4 урок
    Доделал 4 урок
    
    -Сделал усложненное задание
    -Переделал функции в стрелочные
    -Переделал внутринности функций(Убрал объявленные в функции переменные и сразу использовал ранее созданные)

--- js/hardTusk.js
+++ js/hardTusk.js
@@ -1,39 +1,17 @@
-'use strict';
+let str = '             dafkljafkdjklafjkldjklfjakljfdkljaklfjkldjalfjdajfl         ';
 
-let lang = prompt("Введите язык ru или en"),
-    arr,
-    namePerson = prompt("Введите имя");
+const editString = function(a) {
+    if(typeof a === 'number'){
+        return('Не строка');
+    }
+    
+    a = a.replace(/(^\s*)|(\s*)$/g, '');
 
-//Оператор if
-if (lang == "ru") {
-    arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+    if(a.length >= 30) {
+        a= a.slice(0, 30) + '...';
+    }
+
+    return a;
 }
-else if (lang == "en") {
-	arr = ["mn", "tu", "wn", "th", "fr", "st", "sn"];
-}
-console.log ("Месяцы: ", arr);
 
-//Оператор switch
-
-switch (lang) {
-    case "ru":
-        arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
-    break;
-    case "en":
-	    arr = ["mn", "tu", "wn", "th", "fr", "st", "sn"];
-    break;
-}
-console.log ("Месяцы: ", arr);
-
-//Многомерный массив
-
-arr = {
-	"ru":["пн", "вт", "ср","чт", "пт", "сб", "вс"],
-	"en":["mn", "tu", "wn", "th", "fr", "st", "sn"],
-};
-
-console.log ("Месяцы: ", arr[lang]);
-
-let result = namePerson === "Артем" ? "директор" : namePerson === "Максим" ? "преподаватель" : "студент";
-
-console.log (result);
+console.log(editString(str));
--- js/script.js
+++ js/script.js
@@ -10,27 +10,40 @@ let money = +prompt('Сколько ты зарабатываешь?'),
 	expenses1 = prompt('Введите обязательную статью расходов?'),
 	amount1 = +prompt('Во сколько это обойдется?'),
 	expenses2 = prompt('Введите обязательную статью расходов?'),
-	amount2 = +prompt('Во сколько это обойдется?'),
-	budgetMonth = money - amount1 - amount2,
-	period = Math.ceil(mission / budgetMonth),
-	budgetDay = Math.floor(budgetMonth / 30);
-
-const showTypeOf = function (data) {
+	amount2 = +prompt('Во сколько это обойдется?');
+	
+const showTypeOf = (data) => {
     console.log(data, typeof data);
 }
 showTypeOf(money);
 showTypeOf(deposit);
 showTypeOf(income);
+console.log(addExpenses.split(', '));
 
-console.log(addExpenses.length);
+const getExpensesMonth = () => {
+    return amount1 + amount2;
+}
 
+const getAccumulatedMonth = () => {
+    return money - amount1 - amount2;
+}
+
+const getTargetMonth = () => {
+    return mission / accumulatedMonth;
+}
+
+let accumulatedMonth = getAccumulatedMonth(),
+    period = Math.ceil(getTargetMonth()),
+    budgetDay = Math.floor(accumulatedMonth / 30);
+
+console.log ('Сумма всех обязательных расходов: ', getExpensesMonth());
 console.log('Дневной бюджет: ', budgetDay);
-console.log('Бюджет на месяц: ', budgetMonth);
+console.log('Бюджет на месяц: ', accumulatedMonth);
 
 console.log('Цель заработать ' + mission + ' рублей');
 console.log('Период равен ' + period + ' месяцев');
 
-const showMoneyStatus = function () {
+const showStatusIncome = () => {
 	switch (true) {
 		case budgetDay >= 1200:
 			return('У вас высокий уровень дохода');
@@ -46,5 +59,4 @@ const showMoneyStatus = function () {
 			break;
 	}
 }
-
-console.log(showMoneyStatus());
+console.log(showStatusIncome());
